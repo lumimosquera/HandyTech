8a4322aeb6f87ade42bafd5f84aabb3d4aa7958d
Luis Mosquera <lumiluis2018@gmail.com>
2024-06-20 10:23:12 -0500

    se realiza el frontend del footer y del header, configuracion de Views

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -7,7 +7,31 @@ class Home extends Controller
     }
     public function index()
     {
-        $data['title'] = 'Pagina Principal';
+        $data['title'] = 'Inicio';
         $this->views->getView('home', "index", $data);
     }
+    //Vista about
+    public function about()
+    {
+        $data['title'] = 'Acerca de';
+        $this->views->getView('Principal', "about", $data);
+    }
+     //Vista Tienda
+     public function shop()
+     {
+         $data['title'] = 'Productos';
+         $this->views->getView('Principal', "shop", $data);
+     }
+      //Vista detalle producto
+      public function detail($is_producto)
+      {
+          $data['title'] = '---';
+          $this->views->getView('Principal', "detail", $data);
+      }
+       //Vista Contacto
+       public function contactos()
+       {
+           $data['title'] = 'Contactos';
+           $this->views->getView('Principal', "contact", $data);
+       }
 }
